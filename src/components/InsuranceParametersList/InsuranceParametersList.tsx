2658a18d7ccd290a9333619a18595b269c96a67d
oliver.hekmatara <oliver.hekmatara@zingtongroup.com>
2024-09-18 13:42:20 +0200

    update to param list comp

--- src/components/InsuranceParametersList/InsuranceParametersList.tsx
+++ src/components/InsuranceParametersList/InsuranceParametersList.tsx
@@ -8,21 +8,27 @@ interface InsuranceParametersListProps {
 
 export const InsuranceParametersList = ({ parameters }: InsuranceParametersListProps) => {
   return (
-    <ul>
-      {parameters.map((param, index) => (
-        // Need a better key as identifier.
-        <li className={styles.listItem} key={index}>
-          <IconPlus width="20px" />
-          <span className={styles.listItemKey}>{param.parameterDisplayName}</span>
-          <span className={styles.listItemValue}>
-            {param.value === 'true' ? (
-              <IconCheckMark width="24px" color="var(--green)" />
-            ) : (
-              <IconClose width="24px" color="red" />
-            )}
-          </span>
-        </li>
-      ))}
-    </ul>
+    <>
+      {parameters.length ? (
+        <ul>
+          {parameters.map((param, index) => (
+            // Need a better key as identifier.
+            <li className={styles.listItem} key={index}>
+              <IconPlus width="20px" />
+              <span className={styles.listItemKey}>{param.parameterDisplayName}</span>
+              <span className={styles.listItemValue}>
+                {param.value === 'true' ? (
+                  <IconCheckMark width="24px" color="var(--green)" />
+                ) : (
+                  <IconClose width="24px" color="red" />
+                )}
+              </span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>List is empty</p>
+      )}
+    </>
   );
 };
